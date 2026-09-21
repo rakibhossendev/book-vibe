f2446e86137d8f5acbdebf05803273ff680a9fdf
rakibhossendev <rh01828315879@gmail.com>
2026-09-21 22:09:38 +0600

    Remove env file

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -4,7 +4,7 @@ import HeroSection from "../components/Hero/Hero";
 import { Suspense } from "react";
 
 const bookDataPromise = async (): Promise<BookDataType[]> => {
-    const response = await fetch(`${process.env.NEXT_PUBLIC_SITE_URL}/db.json`);
+    const response = await fetch("https://raw.githubusercontent.com/rakibhossendev/book-vibe/main/public/db.json");
     const data = response.json()
     return data;
 }
